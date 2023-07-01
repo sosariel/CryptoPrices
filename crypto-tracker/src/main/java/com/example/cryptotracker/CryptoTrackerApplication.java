adc4c43c7a1c958ff5340473528bf37ee073d4f7
Ariel Sosa <72751687+sosariel@users.noreply.github.com>
2023-06-30 19:01:04 -0500

    Initialized project

---
+++ crypto-tracker/src/main/java/com/example/cryptotracker/CryptoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTrackerApplication.class, args);
+	}
+
+}
